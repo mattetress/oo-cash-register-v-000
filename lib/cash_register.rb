1b30518d7d2fe37f16320b50ea75af74e2fdc4ca
Matt Etress <mattetress@me.com>
2018-10-10 20:03:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,17 @@
 class CashRegister
   attr_accessor :total :discount
-  
+
 
   def initialize(emp_discount = 0)
     @discount = emp_discount
     @total = 0
   end
 
-
-
+  def add_item(item, price, quantity = 1)
+    previous_total = @total
+    @total = @total + price * quantity
+    @items << item
+  end
 
 
 end
